d46d555968229f421b328171e6accc6bfcd4447e
Zuhriddin <zuhriddinzayniddinov99@gmail.com>
2024-04-23 12:23:06 +0500

    Update 7 to 8

--- Entity/obj/Debug/net7.0/Entity.AssemblyInfo.cs
+++ Entity/obj/Debug/net7.0/Entity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d141ccabd9454af2e1d40e0ba9c4c3ccffd5f614")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
