1faeff4a45f28d3cca95712268c1757cc57caf14
hamrakulov2002 <77355381+Khamrakulov-Khojiakbar@users.noreply.github.com>
2025-07-16 23:15:57 +0600

    Added Methods to Country, District, Region controllers (must fix a roles)

--- WebApis/ReferenceBookApi/Controllers/CountryController.cs
+++ WebApis/ReferenceBookApi/Controllers/CountryController.cs
@@ -1,10 +1,22 @@
 using Entity.DataTransferObjects.ReferenceBook;
 using Entity.Models.ReferenceBook;
+using Microsoft.AspNetCore.Mvc;
 using WebCore.Controllers;
 using WebCore.GeneralServices;
+using WebCore.Models;
 
-namespace ReferenceBookApi.Controllers;
-
-public class CountryController(GenericCrudService<Country, CountryDto, long> crudService) : CrudGenericController<Country, CountryDto, long>(crudService)
+public class CountryController(GenericCrudService<Country, CountryDto, long> crudService) : ApiControllerBase
 {
+    [HttpGet]
+    public Task<ResponseModel<List<CountryDto>>> GetAllDistricts([FromQuery] MetaQueryModel metaQueryModel)
+        => crudService.GetAllAsync(metaQueryModel);
+    [HttpGet]
+    public Task<ResponseModel<CountryDto>> GetById([FromRoute]long id)
+        => crudService.GetByIdAsync(id);
+    [HttpPost]
+    public Task<ResponseModel<CountryDto>> OnSave([FromBody] CountryDto dto)
+        => crudService.OnSaveAsync(dto);
+    [HttpPost]
+    public Task<ResponseModel<CountryDto>> Delete([FromBody] long id)
+        => crudService.DeleteByIdAsync(id);
 }
--- WebApis/ReferenceBookApi/Controllers/DistrictController.cs
+++ WebApis/ReferenceBookApi/Controllers/DistrictController.cs
@@ -1,10 +1,23 @@
 using Entity.DataTransferObjects.ReferenceBook;
 using Entity.Models.ReferenceBook;
+using Microsoft.AspNetCore.Mvc;
 using WebCore.Controllers;
 using WebCore.GeneralServices;
+using WebCore.Models;
 
-namespace ReferenceBookApi.Controllers;
-
-public class DistrictController(GenericCrudService<District, DistrictDto, long> crudService) : CrudGenericController<District, DistrictDto, long>(crudService)
+public class DistrictController(GenericCrudService<District, DistrictDto, long> crudService) : ApiControllerBase
 {
+    [HttpGet]
+    public Task<ResponseModel<List<DistrictDto>>> GetAllDistricts([FromQuery] MetaQueryModel metaQueryModel)
+        => crudService.GetAllAsync(metaQueryModel);
+    [HttpGet]
+    public Task<ResponseModel<DistrictDto>> GetById([FromRoute]long id)
+        => crudService.GetByIdAsync(id);
+    [HttpPost]
+    public Task<ResponseModel<DistrictDto>> OnSave([FromBody] DistrictDto dto)
+        => crudService.OnSaveAsync(dto);
+    [HttpPost]
+    public Task<ResponseModel<DistrictDto>> Delete([FromBody] long id)
+    => crudService.DeleteByIdAsync(id);
+    
 }
