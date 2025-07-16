61de672ad1da65ea7a254c18fd2e0dd3b175e4a2
hamrakulov2002 <77355381+Khamrakulov-Khojiakbar@users.noreply.github.com>
2025-07-16 23:15:57 +0600

    Added Methods to Country, District, Region controllers (must fix a roles)

--- WebApis/ReferenceBookApi/Controllers/RegionController.cs
+++ WebApis/ReferenceBookApi/Controllers/RegionController.cs
@@ -1,8 +1,25 @@
 using Entity.DataTransferObjects.ReferenceBook;
 using Entity.Models.ReferenceBook;
+using Microsoft.AspNetCore.Mvc;
 using WebCore.Controllers;
 using WebCore.GeneralServices;
+using WebCore.Models;
 
-public class RegionController(GenericCrudService<Region, RegionDto, long> crudService) : CrudGenericController<Region, RegionDto, long>(crudService)
+public class RegionController(GenericCrudService<Region, RegionDto, long> crudService) : ApiControllerBase
 {
+    [HttpGet]
+    public Task<ResponseModel<List<RegionDto>>> GetAllRegions([FromQuery] MetaQueryModel metaQuery)
+        => crudService.GetAllAsync(metaQuery);
+
+    [HttpGet]
+    public Task<ResponseModel<RegionDto>> GetById([FromRoute] long id)
+        => crudService.GetByIdAsync(id);
+    
+    [HttpPost]
+    public Task<ResponseModel<RegionDto>> OnSave([FromBody] RegionDto regionDto)
+        => crudService.OnSaveAsync(regionDto);
+
+    [HttpDelete]
+    public Task<ResponseModel<RegionDto>> Delete([FromBody] long id)
+        => crudService.DeleteByIdAsync(id);
 }
