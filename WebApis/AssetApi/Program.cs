fdc12229107107b8cc133c4a40987a558d015364
Zuhriddin <zuhriddinzayniddinov99@gmail.com>
2025-09-19 22:53:15 +0500

    fix formate

--- WebApis/AssetApi/Program.cs
+++ WebApis/AssetApi/Program.cs
@@ -12,12 +12,11 @@ var app = builder.Build();
 await app.ConfigureDefault();
 app.UseCors();
 app.UseHttpsRedirection();
-app.UseStaticFiles(new StaticFileOptions()
+app.UseStaticFiles(new StaticFileOptions
 {
     OnPrepareResponse = (context) =>
     {
         var fileName = "Download"+Path.GetExtension(context.File.Name);
-        var header = new ContentRangeHeaderValue(context.File.Length);
         context.Context.Response.Headers.ContentDisposition = $"attachment; filename=\"{fileName}\"";
         context.Context.Response.Headers.ContentType = "application/octet-stream";
         context.Context.Response.Headers[HeaderNames.CacheControl] = $"public, max-age={1 * 24 * 60 * 60}";
